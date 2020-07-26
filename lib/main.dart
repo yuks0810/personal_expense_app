2d5400a307f4603984bc26cbb5d5f454e4cacbbe
yuki.nakata <dorcushopeino1@gmail.com>
2020-07-27 00:42:39 +0900

    - 値段の装飾 - タイトルの装飾 - 日付の装飾

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
+import 'package:personal_expense_app/transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,6 +15,21 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+      id: 't1',
+      title: 'スニーカー',
+      amount: 7000,
+      data: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: '日用品',
+      amount: 12000,
+      data: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,8 +37,9 @@ class MyHomePage extends StatelessWidget {
           title: Text('Flutter App'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly, // Columnのなかでどのようにそれぞれのカードを配置するかを決定する
-          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment
+              .spaceEvenly, // Columnのなかでどのようにそれぞれのカードを配置するかを決定する
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
             Container(
               width: double.infinity, // 横幅いっぱいに広げる
@@ -31,11 +49,57 @@ class MyHomePage extends StatelessWidget {
                 elevation: 5,
               ),
             ),
-            Card(
-              color: Colors.red,
-              child: Text('list of tx'),
+            Column(
+              children: transactions.map((tx) {
+                // mapでリストの数分eachを回しているような感じ
+                return Card(
+                  child: Row(
+                    children: <Widget>[
+                      Container(
+                        margin:
+                            EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                        // 値段の周りにボーダーを表示
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Colors.purple, width: 2),
+                        ),
+                        // 値段の周りにpaddingを追加
+                        padding: EdgeInsets.all(10),
+                        // 商品の値段を表示
+                        child: Text(
+                          // amountはdoubleなのでtoStringでstring型に直す必要がある。
+                          tx.amount.toString(),
+                          // 値段の文字を装飾する
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 13,
+                            color: Colors.purple,
+                          ),
+                        ),
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          // 商品タイトルをColumnのなかに表示
+                          Text(
+                            tx.title,
+                            style: TextStyle(
+                                fontSize: 12, fontWeight: FontWeight.bold),
+                          ),
+                          // 商品の購入日を表示
+                          Text(
+                            tx.data.toString(),
+                            style: TextStyle(color: Colors.grey),
+                          ),
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              }).toList(),
             ),
           ],
         ));
   }
 }
+
+class Colum {}
