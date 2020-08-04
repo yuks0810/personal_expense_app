b5dc57364e0049b2ecba3b7af461ebd4f47443a3
yuki.nakata <dorcushopeino1@gmail.com>
2020-08-04 17:10:24 +0900

    - ListView.builder()でスクロール可能なviewを表示

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -13,55 +13,58 @@ class TransactionList extends StatelessWidget {
     return Container(
       // Containerの高さを指定することでその中でスクロールをすることができる
       // SingleChildScrollViewを設定することでその子要素をスクロールすることができる
+      // SIngleChildScrollView()とListView(children: [])の組み合わせは は見えていない箇所のリストまで表示するのでメモリ負荷が高い
+      // ListView.bui.der() 見えていないリストは表示されないのでメモリ負荷が低い
       height: 300,
-      child: SingleChildScrollView(
-              child: Column(
-          children: transactions.map((tx) {
-            // mapでリストの数分eachを回しているような感じ
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    // 値段の周りにボーダーを表示
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple, width: 2),
-                    ),
-                    // 値段の周りにpaddingを追加
-                    padding: EdgeInsets.all(10),
-                    // 商品の値段を表示
-                    child: Text(
-                      // amountはdoubleなのでtoStringでstring型に直す必要がある。
-                      '${tx.amount}円', // '\$${tx.amount}',
-                      // 値段の文字を装飾する
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 13,
-                        color: Colors.purple,
-                      ),
+      child: ListView.builder(
+        // itemBuilderでindexとそれぞれのtransactionsをeachで回しているような感じ
+        itemBuilder: (ctx, index) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                  // 値段の周りにボーダーを表示
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.purple, width: 2),
+                  ),
+                  // 値段の周りにpaddingを追加
+                  padding: EdgeInsets.all(10),
+                  // 商品の値段を表示
+                  child: Text(
+                    // amountはdoubleなのでtoStringでstring型に直す必要がある。
+                    '${transactions[index].amount}円', // '\$${tx.amount}',
+                    // 値段の文字を装飾する
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 13,
+                      color: Colors.purple,
                     ),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      // 商品タイトルをColumnのなかに表示
-                      Text(
-                        tx.title,
-                        style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
-                      ),
-                      // 商品の購入日を表示
-                      Text(
-                        DateFormat.yMd().add_jm().format(tx
-                            .date), // import 'package:intl/intl.dart';でimportした日付フォーマットを使っている
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  )
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    // 商品タイトルをColumnのなかに表示
+                    Text(
+                      transactions[index].title,
+                      style:
+                          TextStyle(fontSize: 12, fontWeight: FontWeight.bold),
+                    ),
+                    // 商品の購入日を表示
+                    Text(
+                      DateFormat.yMd().add_jm().format(transactions[index]
+                          .date), // import 'package:intl/intl.dart';でimportした日付フォーマットを使っている
+                      style: TextStyle(color: Colors.grey),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          );
+        },
+        // 何個のリストがあるかを指定する
+        itemCount: transactions.length,
       ),
     );
   }
